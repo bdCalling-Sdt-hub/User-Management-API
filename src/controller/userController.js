adf79254d2a4251dbfadea13500e7ac186afca59
unknown <towhidulsislam@gmail.com>
2023-07-08 16:54:20 +0600

    User pagination and search

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,9 +1,43 @@
 const createError = require('http-errors');
+const User = require('../models/userModel');
 
-const getUsers = (req, res, next) => {
+const getUsers = async(req, res, next) => {
     try {
+        const  search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 1;
+
+        const searchRegExp = new RegExp('.*' + search + '.*', 'i');
+
+        const filter = {
+            isAdmin: { $ne:true },
+            $or: [
+                {name: {$regex: searchRegExp}},
+                {email: {$regex: searchRegExp}},
+                {phone: {$regex: searchRegExp}},
+            ]
+        };
+
+        const option = {
+            password: 0
+        }
+
+        const users = await User.find(filter, option).limit(limit).skip((page - 1) * limit);
+        const count = await User.find(filter).countDocuments();
+        
+        if(!users){
+            throw createError(404, 'User not found');
+        };
+
         res.status(200).send({
             message: "User returned succesfully",
+            users,
+            pagination: {
+                totalPages: Math.ceil(count / limit),
+                currentPage: page,
+                previousPage: page - 1 > 0 ? page - 1 : null,
+                nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+            }
         });
     } catch (error) {
         next(error);
